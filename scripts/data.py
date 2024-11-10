660915e71d0c611f1c39b8c86be71688e9481dca
akadouri <ariel@arielsartistry.com>
2024-11-10 14:56:06 -0500

    Update bench/park data

--- scripts/data.py
+++ scripts/data.py
@@ -1,16 +1,21 @@
 """
 Query local OpenStreetMap postgresql database.
 """
+
 from os.path import abspath, join
 import psycopg2
 from json import dumps
 
-PARKS_QUERY = """
+BOUNDARY = "st_geomfromtext('Polygon ((-8266094.70446772 4938303.187610116, -8266094.70446772 4999890.688531854, -8204247.500130178 4999890.688531854, -8204247.500130178 4938303.187610116, -8266094.70446772 4938303.187610116))', 3857 )"
+DATE_QUERY = (
+    "select value from osm2pgsql_properties where property = 'import_timestamp'"
+)
+PARKS_QUERY = f"""
     with parks as (
         select name, osm_id, st_multi(st_collect(planet_osm_polygon.way)) as way
-        from planet_osm_polygon, nycd
+        from planet_osm_polygon
         where planet_osm_polygon.leisure = 'park'
-        and st_within(planet_osm_polygon.way, nycd.geom)
+        and st_within(planet_osm_polygon.way, {BOUNDARY})
         group by name, osm_id
     ), query as (
         select parks.osm_id, parks.name, 
@@ -25,34 +30,34 @@ PARKS_QUERY = """
     ) SELECT jsonb_build_object(
         'type',     'FeatureCollection',
         'features', jsonb_agg(feature),
-        'importdate', (select importdate from planet_osm_replication_status)
+        'importdate', ({DATE_QUERY})
     )
     FROM (
     SELECT jsonb_build_object(
         'type',       'Feature',
         'id',         osm_id,
-        'geometry',   ST_AsGeoJSON(way)::jsonb,
+        'geometry',   ST_AsGeoJSON(way, 5)::jsonb,
         'properties', to_jsonb(row) - 'gid' - 'way'
     ) AS feature
     FROM (SELECT * FROM query) row) features;
 """
 
-BENCHES_QUERY = """
+BENCHES_QUERY = f"""
     with query as (
 	select osm_id, way
-    from planet_osm_point, nycd
+    from planet_osm_point
     where amenity = 'bench'
-    and st_intersects(planet_osm_point.way, nycd.geom)
+    and st_intersects(planet_osm_point.way, {BOUNDARY})
     ) SELECT jsonb_build_object(
         'type',     'FeatureCollection',
         'features', jsonb_agg(feature),
-        'importdate', (select importdate from planet_osm_replication_status)
+        'importdate', ({DATE_QUERY})
     )
     FROM (
     SELECT jsonb_build_object(
         'type',       'Feature',
         'id',         osm_id,
-        'geometry',   ST_AsGeoJSON(st_transform(way, 4326))::jsonb,
+        'geometry',   ST_AsGeoJSON(st_transform(way, 4326), 5)::jsonb,
         'properties', to_jsonb(row) - 'osm_id' - 'way'
     ) AS feature
     FROM (SELECT * FROM query) row) features;
