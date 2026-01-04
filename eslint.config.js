d8f68dbf2a759082df562897037cc39818f286f9
Ariel Kadouri <ariel@arielsartistry.com>
2026-01-04 13:28:28 -0500

    Update eslint and depedencies

---
+++ eslint.config.js
@@ -0,0 +1,31 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import pluginVue from "eslint-plugin-vue";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,vue}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+  },
+  tseslint.configs.recommended,
+  pluginVue.configs["flat/essential"],
+  {
+    files: ["**/*.vue"],
+    languageOptions: { parserOptions: { parser: tseslint.parser } },
+  },
+  {
+    name: "app/custom-rules",
+    files: ["**/*.{ts,mts,tsx,vue}"],
+    rules: {
+      "vue/multi-word-component-names": "off",
+    },
+  },
+  {
+    name: "app/ignores",
+    ignores: ["**/dist/**", "**/dist-ssr/**", "**/coverage/**"],
+  },
+]);
